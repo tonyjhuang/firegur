762c6ab73eb4b02a4c58c4d51b035bc675cf3d7c
Tony Huang <tony.huang.jun@gmail.com>
2020-11-17 14:56:16 -0800

    basic styling for new post page

--- public/src/index.ts
+++ public/src/index.ts
@@ -3,19 +3,8 @@ export {};
 import firebase from '@firebase/app'
 import '@firebase/analytics'
 import '@firebase/auth'
+import {firebaseConfig} from './firebase_config'
 
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-    apiKey: "AIzaSyADkx0R20kiGU7upgsdFGn1PORCYs_Sps8",
-    authDomain: "firegur-app.firebaseapp.com",
-    projectId: "firegur-app",
-    storageBucket: "firegur-app.appspot.com",
-    messagingSenderId: "276908818311",
-    appId: "1:276908818311:web:a9d98565066ab458fe1d22",
-    measurementId: "G-2BB8V06QH4"
-};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
--- public/src/new.ts
+++ public/src/new.ts
@@ -1,33 +1,53 @@
-export {};
+export { };
 
 import $ from 'jquery';
-/*
-// show uploaded image
-function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
 
-        reader.onload = function (e) {
-            $('#imageResult')
-                .attr('src', e.target.result);
-        };
-        reader.readAsDataURL(input.files[0]);
+var formState = {
+    title: false,
+    image: false
+};
+
+$('#upload').on('change', function (e) {
+    const input = e.currentTarget as HTMLInputElement
+    if (input.files && input.files[0]) {
+        const label = $('#upload-label')[0] as HTMLLabelElement;
+        const file = input.files[0];
+        label.innerText = file.name;
+        setImagePreview($('#imageResult')[0] as HTMLImageElement, file)
+    }
+});
+
+function setImagePreview(preview: HTMLImageElement, file: File) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        const target =  e.target;
+        if (!target) return;
+        const result = target.result;
+        if (!result) return;
+        preview.src = result as string
+        formState.image = true
+        updateViewState();
+    };
+    reader.readAsDataURL(file);
+}
+$('#title').on('input', function (e) {
+    const input = e.currentTarget as HTMLInputElement;
+    console.log(input.value);
+    formState.title = !!input.value;
+    updateViewState();
+});
+
+function updateViewState() {
+    console.log(JSON.stringify(formState));
+    const submit = $('#submit')[0] as HTMLButtonElement;
+    if (formState.title && formState.image) {
+        submit.disabled = false;
+    } else {
+        submit.disabled = true;
     }
 }
 
-$(function () {
-    $('#upload').on('change', function () {
-        readURL(input);
-    });
+$('#submit').on('click', function(e) {
+    console.log('hello!');
 });
-
-var input = document.getElementById('upload');
-var infoArea = document.getElementById('upload-label');
-
-input.addEventListener('change', showFileName);
-
-function showFileName(event) {
-    var input = event.srcElement;
-    var fileName = input.files[0].name;
-    infoArea.textContent = 'File name: ' + fileName;
-}*/
