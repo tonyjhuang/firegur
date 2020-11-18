1cc9123c42f2cd60ec0092d2340a4089fd97e5cc
jacqtdoan <jacqtdoan@google.com>
2020-11-17 18:37:29 -0800

    Move code from signin.ts to index.ts

--- public/src/signin.ts
+++ public/src/signin.ts
@@ -5,8 +5,6 @@ import "firebase/auth"
 import * as firebaseui from "firebaseui";
 import $ from 'jquery';
 import { firebaseApp } from './firebase_config'
-import { UserService } from './user_service'
-
 
 var ui = new firebaseui.auth.AuthUI(firebaseApp.auth());
 
@@ -37,36 +35,3 @@ var uiConfig = {
 };
 
 ui.start("#firebaseui-auth-container", uiConfig);
-
-var currentUid: string | null = null;
-firebase.auth().onAuthStateChanged(function(user) {
-    var userService = new UserService();
-    if (user && user.uid != currentUid) {
-        // Hide sign-in button and show sign-out button
-        // if user.uid exists in storage
-        // else, createUser
-        if (!userService.isUserRegistered(user.uid)) {
-            var username = "";
-            if (user.displayName) {
-                username = user.displayName;
-            } else if (user.displayName)
-            userService.newUser(
-                user.uid,
-                {username: username},
-                {
-                    onComplete() {
-                        console.log('Registered user!')
-                    },
-                    onError(e: Error) {
-                        console.warn(e);
-                    }
-
-                });
-        }
-    } else { 
-        // Sign out operation. Reset current user UID.
-        currentUid = null;
-        // Hide sign-out button and show sign-in button in index.html
-    }
-
-});
