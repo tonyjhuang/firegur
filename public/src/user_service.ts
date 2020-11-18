d2a4ff96a925b7b002c77cb75584e2f2bbb305d1
jacqtdoan <jacqtdoan@google.com>
2020-11-18 15:09:53 -0800

    Convert UserService to be asynchronous

--- public/src/user_service.ts
+++ public/src/user_service.ts
@@ -18,10 +18,10 @@ export interface NewUserCallback {
 
 export class UserService {
     /**
-     * Uploads an image and creates a new Post for it.
+     * Creates and stores a new user.
      */
-    newUser(userId: string, options: CreateUserOptions, callback: NewUserCallback) {
-        createUser(userId, options)
+    async newUser(userId: string, options: CreateUserOptions, callback: NewUserCallback) {
+        await createUser(userId, options)
             .then(callback.onComplete)
             .catch(callback.onError);
     }
@@ -29,7 +29,7 @@ export class UserService {
     /**
      * Checks if the user exists in Firestore.
      */ 
-    isUserRegistered(id: string) : boolean {
+    async isUserRegistered(id: string) : Promise<boolean> {
         const firestore = firebaseApp.firestore();
         var docRef = firestore.collection('users').doc(id);
         docRef.get().then(function(doc) {
@@ -45,7 +45,7 @@ export class UserService {
 
     // TODO: Implement adding group to user.
     addGroupToUser(userId: string, groupIds: String[]) {
-        // Check for user
+        // Get user's reference in Firestore
         // Add groupIds to user's array
     }
 
@@ -65,7 +65,7 @@ export class UserService {
 /**
  * Creates a new User document in Firestore.
  */
-function createUser(userId: string, options: CreateUserOptions) {
+function createUser(userId: string, options: CreateUserOptions): Promise<void> {
     const firestore = firebaseApp.firestore();
     const data: any = {
         username: options.username,
