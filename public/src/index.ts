f82eeb1051a7e877b137fd0af9116c945301ce9c
jacqtdoan <jacqtdoan@google.com>
2020-11-17 18:36:33 -0800

    Store user in Firestore once signed in

--- public/src/index.ts
+++ public/src/index.ts
@@ -3,6 +3,7 @@ export { };
 import '@firebase/analytics'
 import '@firebase/auth'
 import { firebaseApp } from './firebase_config'
+import { UserService } from './user_service'
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -41,4 +42,38 @@ document.addEventListener('DOMContentLoaded', function () {
         console.error(e);
         loadEl.textContent = 'Error loading the Firebase SDK, check the console.';
     }
+});
+
+var currentUid: string | null = null;
+firebaseApp.auth().onAuthStateChanged(function(user) {
+    var userService = new UserService();
+    if (user && user.uid != currentUid) {
+        // TODO: Hide sign-in button and show sign-out button
+        // if user id not in database, create user
+        if (!userService.isUserRegistered(user.uid)) {
+            var username = "";
+            if (user.displayName) {
+                username = user.displayName;
+            } else if (user.email) {
+                username = user.email;
+            }
+            userService.newUser(
+                user.uid,
+                {username: username},
+                {
+                    onComplete() {
+                        console.log('Registered user!')
+                    },
+                    onError(e: Error) {
+                        console.warn(e);
+                    }
+
+                });
+        }
+    } else { 
+        // TODO: Sign out operation. Reset current user UID.
+        currentUid = null;
+        // TODO: Hide sign-out button and show sign-in button in index.html
+    }
+
 });
