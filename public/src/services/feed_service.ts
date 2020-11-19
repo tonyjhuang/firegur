27801c7a399cf9e4614b4969b57dfba85d82419b
Tony Huang <tony.huang.jun@gmail.com>
2020-11-19 14:54:26 -0800

    fix spinner

--- public/src/services/feed_service.ts
+++ public/src/services/feed_service.ts
@@ -31,13 +31,13 @@ async function renderPosts(postsSnapshot: firebase.firestore.QuerySnapshot<fireb
             try {
                 const post = await new PostService().get(postId);
                 console.log(JSON.stringify(post));
-                hideSpinner();
                 $('#feed-container').append(await new PostRenderer().renderPost(post, postId, /* isFeedPost= */ true));
             } catch (e) {
                 alert((e as Error).message);
             }
         }
     });
+    hideSpinner();
 }
 
 function hideSpinner() {
